a2c23e83219544737820a42dbee70397c8a98cf2
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-01-23 08:55:36 +0100

    error msg

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -15,14 +15,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     }
 
   if (err.name === "CastError") {
-    customError.msg = `No talk with id found`;
+    customError.msg = `No talk with id found.`;
     customError.statusCode = 404;
   }
 
   if (err.code && err.code === 11000) {
     customError.msg = `${Object.keys(
       err.keyValue
-    )} already exist. Please use another ${Object.keys(err.keyValue)}`;
+    )} already exist. Please use another ${Object.keys(err.keyValue)}.`;
     customError.statusCode = 400;
   }
 
